6853569caee3dfa2fede9cb361e19e4ecbfb5cc0
Dominic <dominiceharvey@gmail.com>
2024-11-21 15:50:11 +0000

    Implemented Api Contract

--- src/main/java/com/eventscheduler/exception/GlobalExceptionHandler.java
+++ src/main/java/com/eventscheduler/exception/GlobalExceptionHandler.java
@@ -6,12 +6,17 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @ControllerAdvice
 @Slf4j
 public class GlobalExceptionHandler {
     @ExceptionHandler(EventNotFoundException.class)
-    public ResponseEntity<String> handleEventNotFoundException(EventNotFoundException ex) {
+    public ResponseEntity<Object> handleEventNotFoundException(EventNotFoundException ex) {
         log.error(ex.getMessage(), ex);
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
+        Map<String, String> errorResponse = new HashMap<>();
+        errorResponse.put("message", ex.getMessage());
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(errorResponse);
     }
 }
