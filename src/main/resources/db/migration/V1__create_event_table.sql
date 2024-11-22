d0e4a485920a6ae3e83aa72659b3311aaa2f9ee1
Dominic <dominiceharvey@gmail.com>
2024-11-22 12:30:46 +0000

    added index for stand and end time in event table

--- src/main/resources/db/migration/V1__create_event_table.sql
+++ src/main/resources/db/migration/V1__create_event_table.sql
@@ -1,9 +1,11 @@
 CREATE TABLE events
 (
     id         BIGINT AUTO_INCREMENT PRIMARY KEY,
-    name       VARCHAR(255)                        NOT NULL,
-    start_time TIMESTAMP                           NOT NULL,
-    end_time   TIMESTAMP                           NOT NULL,
-    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+    name       VARCHAR(255) NOT NULL,
+    start_time TIMESTAMP    NOT NULL,
+    end_time   TIMESTAMP    NOT NULL,
+    created_at TIMESTAMP    NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP    NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 );
+
+CREATE INDEX idx_event_time_range ON events (start_time, end_time);
