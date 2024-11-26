8c61e4e0e918caa22a208f19ea1678681e9e9844
Dominic <dominiceharvey@gmail.com>
2024-11-26 20:07:13 +0000

    fixed issue with EventDto deserialization, update example dates in api spec to be more useful

--- src/main/java/com/eventscheduler/dto/EventDto.java
+++ src/main/java/com/eventscheduler/dto/EventDto.java
@@ -3,21 +3,25 @@ package com.eventscheduler.dto;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 
 @Builder
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class EventDto {
-    private final Long id;
+    private Long id;
     @NotBlank(message = "Event name must not be blank")
     @Size(min = 1, max = 255, message = "Event name must be between 1 and 255 characters")
-    private final String name;
+    private String name;
     @NotNull(message = "Start time must not be null")
-    private final LocalDateTime startTime;
+    private LocalDateTime startTime;
     @NotNull(message = "End time must not be null")
-    private final LocalDateTime endTime;
+    private LocalDateTime endTime;
 }
 
